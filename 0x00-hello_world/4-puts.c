896c30b1b6ce9194e1a58eafb6e3b92c711aa3df
BrandyMosala <mabrandmosala@gmail.com>
2023-04-20 07:29:55 -0700

    wrote a C program using the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program execution
+ * return: is 0 - true
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
