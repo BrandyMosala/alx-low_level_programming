e7a21e55f93259f58336a20cce23bcf0d6a26f49
BrandyMosala <mabrandmosala@gmail.com>
2023-04-21 10:06:23 -0700

    wrote a program that prints all singles digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'prints all single digits'
+ *Return: Always 0
+ */
+int main(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
+}
