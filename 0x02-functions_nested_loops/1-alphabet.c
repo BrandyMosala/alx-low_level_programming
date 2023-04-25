03768ed078399e3e4a10642cf0ad2fd51445b532
BrandyMosala <mabrandmosala@gmail.com>
2023-04-25 04:23:34 -0700

    updated a function that prints alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,18 +5,11 @@
  */
 void print_alphabet(void)
 {
-int a;
+char a;
 for (a = 97; a <= 122; a++)
+{
 _putchar(a);
+}
 _putchar('\n');
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+
