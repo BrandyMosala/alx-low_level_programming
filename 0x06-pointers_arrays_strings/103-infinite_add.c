0744519a8c422dcf622519d235f2c987d9e4090c
BrandyMosala <mabrandmosala@gmail.com>
2023-05-04 09:55:41 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+int a = 0;
+int b = 0;
+char temp;
+while (*(n + a) != '\0')
+{
+a++;
+}
+a--;
+for (b = 0; b < a; b++, a--)
+{
+temp = *(n + b);
+*(n + b) = *(n + a);
+*(n + a) = temp;
+}
+}
