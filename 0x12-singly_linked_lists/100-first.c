e4fef6db25ceb270c3c5eaab15afcbd6a7cef938
BrandyMosala <mabrandmosala@gmail.com>
2023-06-01 09:48:21 -0700

    wrote a function that prints a sentsence before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * first_sent - a function that  prints a sentence before main
+ */
+void first_sent(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
