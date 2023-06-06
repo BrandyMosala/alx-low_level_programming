f9bd822ffd81b9894373b159b4305b73abf3455f
BrandyMosala <mabrandmosala@gmail.com>
2023-06-06 09:24:40 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,71 +1,25 @@
 #include "lists.h"
-#include <stdio.h>
-
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
-
-/**
- * looped_listint_len - a function that counts number of unique nodes
- * @head: pointer to the head of listint_t
- * Return: If the list is not looped - 0
- */
-size_t looped_listint_len(const listint_t *head)
-{
-const listint_t *tortoise, *hare;
-size_t nodes = 1;
-if (!head || head->next == NULL)
-return (0);
-tortoise = head->next;
-hare = (head->next)->next;
-while (hare)
-{
-if (tortoise == hare)
-{
-tortoise = head;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-hare = hare->next;
-}
-tortoise = tortoise->next;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-}
-return (nodes);
-}
-tortoise = tortoise->next;
-hare = (hare->next)->next;
-return (0);
-}
-
 /**
  * print_listint_safe - a function prints a listint_t list safely
- * @head: a pointer to the head of the listint_t list.
- * Return:  number of nodes in the list.
+ * @head: a pointer
+ * Return:  number of nodes in list
  */
-size_t print_listint_safe(const listint_t *head)
+  size_t print_listint_safe(const listint_t *head)
 {
-size_t nodes, indx = 0;
-nodes = looped_listint_len(head);
-if (nodes == 0)
+/*decalre node and index */
+size_t number = 0;
+long int diff;
+while (head)
 {
-for (; head != NULL; nodes++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
+diff = head - head->next;
+number++;
+if (diff > 0)
 head = head->next;
-}
-}
 else
 {
-for (indx = 0; indx < nodes; indx++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+break;
 }
-printf("-> [%p] %d\n", (void *)head, head->n);
 }
-return (nodes);
+return (number);
 }
